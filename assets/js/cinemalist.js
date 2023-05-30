2cf33d4c108ed878ecd3cb34662a5905d48761d4
sapariyadhara <sapariyadhara935@gmail.com>
2023-05-30 22:58:02 +0530

    cinema list add

--- assets/js/cinemalist.js
+++ assets/js/cinemalist.js
@@ -10,30 +10,39 @@ const handleCinemaName = () => {
     let cname = getAllMovie.filter((v) => v.mName === getMName)
     console.log(cname);
 
-    let Cname1 = getCdata.filter((v) => v.name === cname[0].Cname)
-    console.log(Cname1);
+    // let Cname1 = getCdata.filter((v) => v.name === cname[0].Cname)
+    // console.log(Cname1);
 
     cname.map((v) => {
-       
-       let temp = cname
-       console.log(temp[0].cid);
-       
-        
+        let temp = []
+        temp.push({
+            cid: v.cid
+        })
+        console.log(temp);
+
+        getCdata.map((m) => {
+            console.log(m.cid , v.cid);
+            if (m.cid == v.cid) {
+                console.log(m.cid == v.cid);
+                let temp1 = []
+                temp1.push(m)
+                console.log(temp1);
+                let disp = ''
+                temp1.map((t) => {
+                    disp += ' <div id="mImg"><img src=' + '../assets/images/' + t.imageC + '></div>'
+                    disp += '<div>'
+                    disp += ' <h1>' + t.name + '</h1>'
+                    disp += '<h3>' + t.location + '</h3>'
+                    disp += '<h3>' + t.facility + '</h3>'
+                })
+                document.getElementById("cList").innerHTML = disp
+               
+            }
+
+        })
+
     })
-    // Cname1.map((v) => {
-       
-    //         let disp = ''
-    //         Cname1.map((v) => {
-    //             disp += ' <div id="cImg"><img src=' + '../assets/images/' + v.imageC + '></div>'
-    //             disp += '<div>'
-    //             disp += ' <h1>' + v.Cname + '</h1>'
-    //             disp += ' <h3>' + v.location + '</h3>'
-    //             disp += ' <p>' + v.facility + '</p>'
-    //             disp += '</div>'
-    //         })
-    //         document.getElementById("cList").innerHTML = disp
-     
-    // })
+  
 
 }
 
