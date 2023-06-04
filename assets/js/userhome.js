b010f65fe90610c9ab84d7c824f4b43e559f4ee8
sapariyadhara <sapariyadhara935@gmail.com>
2023-06-04 19:06:57 +0530

    userhome complete

--- assets/js/userhome.js
+++ assets/js/userhome.js
@@ -16,49 +16,35 @@ const getCMUserSide = (data) => {
     if (getCinemaData != null) {
         let disp = ''
         getCinemaData.map((v) => {
-            disp += '<button  onclick="handleCinemaDetails('+ v.cid +')" >'
-            disp += ' <div id=' + "data-" + v.cid + ' class="col-12 col-sm-6 col-lg-3">'
-            disp += ' <div  class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" >'
-            disp += ' <div class="advisor_thumb"><img src=' + '../assets/images/' + v.imageC + '>'
-            disp += ' <div  class="social-info">' + v.name + '</div>'
-            disp += ' <div class="single_advisor_details_info">' + v.location + '</div>'
-            disp += ' <div class="single_advisor_details_info">' + v.facility + '</div>'
-            disp += '</div>'
+           
+            disp += ' <div id=' + "data-" + v.cid + ' class="">'
+            disp += '<button id="cbtn1"  onclick="handleCinemaDetails('+ v.cid +')" >'
+            disp += ' <div  class="" >'
+            disp += ' <div class="cineimg"><img src=' + '../assets/images/' + v.imageC + '>'
+            disp += ' <div  class="t1">' + 'Cinema : ' +v.name + '</div>'
+            disp += ' <div class="t1">' + 'Location : ' +v.location + '</div>'
+            disp += ' <div class="t1">' + 'Facility : ' + v.facility + '</div>'
             disp += '</div>'
             disp += '</div>'
             disp += '</button>'
+            disp += '</div>'
+           
         })
         document.getElementById("trCine").innerHTML = disp
     }
 
-    // if (getCinemaData != null) {
-    //     let disp = ''
-    //     getCinemaData.map((v) => {
-    //         disp += ' <div  class="swiper mySwiper">'
-    //            disp += ' <div id=' + "data-" + v.cid + ' class="swiper-wrapper">'
-    //         // disp += '<button  onclick="handleCinemaDetails('+ v.cid +')" >'
-    //         disp += '<div class="swiper-slide"> <img src=' + '../assets/images/' + v.imageC + '>'
-    //         // disp += ' <div  class=" ">' + v.name + '</div>'
-    //         // disp += ' <div class="">' + v.location + '</div>'
-    //         // disp += ' <div class="">' + v.facility + '</div>'
-    //         disp += '</div>'
-    //         // disp += '</button>'
-    //          disp += ' </div>'
-    //          disp += '</div>'
-    //     })
-    //     document.getElementById("trCine").innerHTML = disp
-    // }
+ 
   
     if (getMovieData != null) {
         let disp = ''
         getMovieData.map((v) => {
-            disp += '<button  onclick="handleMovieDetails('+ v.mid +')" >'
-            disp += ' <div id=' + "data-" + v.mid + ' class="col-12 col-sm-6 col-lg-3">'
-            disp += ' <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" >'
-            disp += ' <div class="advisor_thumb"><img src=' + '../assets/images/' + v.poster + '>'
-            disp += ' <div class="social-info">' + v.mName + '</div>'
-            disp += ' <div class="single_advisor_details_info">' + v.Cname + '</div>'
-            disp += ' <div class="single_advisor_details_info">' + v.decrpt + '</div>'
+            disp += '<button id="mbtn1"  onclick="handleMovieDetails('+ v.mid +')" >'
+            disp += ' <div id=' + "data-" + v.mid + ' class="">'
+            disp += ' <div class=""  >'
+            disp += ' <div class="movimg"><img src=' + '../assets/images/' + v.poster + '>'
+            disp += ' <div class="m1">' + v.mName + '</div>'
+            disp += ' <div class="m1">' + v.Cname + '</div>'
+            disp += ' <div class="m1">' + v.decrpt + '</div>'
             disp += '</div>'
             disp += '</div>'
             disp += '</div>'
