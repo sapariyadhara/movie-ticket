42ca663fb2f8d01d25f5365dcddde050b713516b
sapariyadhara <sapariyadhara935@gmail.com>
2023-06-04 14:29:01 +0530

    moviedetail page complete

--- assets/js/moviedetails.js
+++ assets/js/moviedetails.js
@@ -9,20 +9,39 @@ const handleMoviesDetails = () => {
     let mData = getMoviD.filter((v) => v.mid === getMid)
     console.log(mData);
 
+    // if (mData ) {
+
+    //     let disp = ''
+    //     mData.map((v) => {
+    //         disp += ' <div id="mImg"><img src=' + '../assets/images/' + v.poster + '></div>'
+    //         disp += '<div>'
+    //         disp += ' <h1>' + v.mName + '</h1>'
+    //         disp += ' <h3>' + v.Cname + '</h3>'
+    //         disp += ' <p>' + v.decrpt + '</p>'
+    //         disp += ' <p>' + v.time  + '</p>'
+    //         disp += `<button onclick="handleCinemaData('${v.mName}')"> Book Ticket </button>`
+    //         disp += '</div>'
+    //     })
+    //     document.getElementById("movieD").innerHTML = disp
+    // }
+    
     if (mData ) {
 
         let disp = ''
         mData.map((v) => {
-            disp += ' <div id="mImg"><img src=' + '../assets/images/' + v.poster + '></div>'
-            disp += '<div>'
+            disp += ' <div class="imgdata"><img src=' + '../assets/images/' + v.poster + '></div>'
+            disp += '<div class="data">'
             disp += ' <h1>' + v.mName + '</h1>'
-            disp += ' <h3>' + v.Cname + '</h3>'
-            disp += ' <p>' + v.decrpt + '</p>'
-            disp += ' <p>' + v.time  + '</p>'
-            disp += `<button onclick="handleCinemaData('${v.mName}')"> Book Ticket </button>`
+            disp += ' <h3>' + 'Cinema : ' + v.Cname + '</h3>'
+            disp += ' <p>' + 'Description : ' + v.decrpt + '</p>'
+            disp += ' <p>' + ' <i class="fa-solid fa-star"></i>'+ ' 8.7/10 '+ '<span>'+ '50.30K Votes ' +'<i class="fa-solid fa-angle-right "></i>'  + '</span>'+'</p>'
+
+            disp += '<button > 2D </button>'+'<button > 3D </button>' + '<button > Hindi </button>' + '<button > English </button>'
+            disp += ' <p>' + 'Time : ' + v.time  + '</p>'
+            disp += `<button id="bookb" onclick="handleCinemaData('${v.mName}')"> Book Ticket </button>`
             disp += '</div>'
         })
-        document.getElementById("movieD").innerHTML = disp
+        document.getElementById("container1").innerHTML = disp
     }
 
 }
