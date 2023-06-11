a1cd0ec33227f372d54c936ee14b658fbf3e257b
sapariyadhara <sapariyadhara935@gmail.com>
2023-06-11 19:04:57 +0530

    admin page

--- assets/js/adminhome.js
+++ assets/js/adminhome.js
@@ -17,3 +17,20 @@ const handleMovie = () => {
 const handleSeat = () => {
     window.location = "./addseat.html"
 }
+
+const handleCineMovi = () => {
+    let getCinema = JSON.parse(localStorage.getItem("cinema"))
+    let getMovie = JSON.parse(localStorage.getItem("movie"))
+    console.log(getCinema);
+    for(let i = 0 ; i < getCinema.length ; i ++){
+        console.log(getCinema.length);
+        document.getElementById("cl").innerHTML = getCinema.length
+    }
+
+    for(let i = 0 ; i < getMovie.length ; i ++){
+        console.log(getMovie.length);
+        document.getElementById("ml").innerHTML = getMovie.length
+    }
+}
+
+window.onload = handleCineMovi
